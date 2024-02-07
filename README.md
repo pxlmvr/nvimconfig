3a1bd8bd6fa33091682471e62b6e53ed874e6d51
pxlmvr <matteo.losi@casavo.com>
2024-02-07 12:03:29 +0100

    update: add dependencies section to Readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Nvimconfig
 
-sync with `~/.config/nvim/`
+Configurations usually live in `~/.config/nvim/`.
+
+## Dependencies
+
+### Telescope
+
+Ensure [ripgrep](https://github.com/BurntSushi/ripgrep?tab=readme-ov-file#installation) is installed.
--- init.lua
+++ init.lua
@@ -36,6 +36,14 @@ require("lazy").setup("plugins")
 local lspconfig = require('lspconfig')
 
 -- Controls for Telescope
+require('telescope').setup{ 
+  defaults = { 
+    file_ignore_patterns = { 
+      "node_modules" 
+    }
+  }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
